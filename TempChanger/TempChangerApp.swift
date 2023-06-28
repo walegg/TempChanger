d8f86f7353461ec34f27503e73503f1a240c865c
Louis <mille.louis1@gmail.com>
2023-06-28 16:02:37 +0200

    Initial Commit

---
+++ TempChanger/TempChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempChangerApp.swift
+//  TempChanger
+//
+//  Created by Louis Mille on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TempChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
